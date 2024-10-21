647b2ecbcd8c89843979d1f222275889c9e5830c
Nikita1380 <nik138013.yandex.ru>
2024-10-21 11:28:41 +0300

    init project

---
+++ AvitoTechTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTechTestTask
+//
+//  Created by Никита Козловский on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
